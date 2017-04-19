546a914c2b8ddbdb02aee0a05dce67564d6b39ce
jaequery <jaequery@gmail.com>
2017-04-19 10:31:32 -0700

    created bin for utility scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 parentdir="$(dirname "$dir")"
 app=${parentdir##*/}
 dest=$1
-dest="jae@x.hakberry.com"
+dest="user@yourserver.com"
 rsync --exclude="volumes" --exclude=".git" -avzr $parentdir $dest:~/sites/ && ssh $dest "cd ~/sites/$app && docker-compose kill && docker-compose -f docker-compose.live.yml up && docker-compose logs"
